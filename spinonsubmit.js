401aee1fd482e7d2006f791e49b8a144d517160a
Restyled.io <commits@restyled.io>
2023-05-25 14:04:30 +0000

    Restyled by prettier

--- spinonsubmit.js
+++ spinonsubmit.js
@@ -6,12 +6,19 @@ function resetButton(button, spinner, buttonLabel, hideLabelWhileLoading) {
   }
 }
 
-export function createSpinnerButton(buttonId, formId, onSubmit, onError,
-                                    spinnerColor = 'black', position = 'left',
-                                    hideLabelWhileLoading = true) {
+export function createSpinnerButton(
+  buttonId,
+  formId,
+  onSubmit,
+  onError,
+  spinnerColor = "black",
+  position = "left",
+  hideLabelWhileLoading = true
+) {
   if (!buttonId || !formId || !onSubmit) {
     throw new Error(
-        'Missing required parameters: buttonId, formId and onSubmit are required.');
+      "Missing required parameters: buttonId, formId and onSubmit are required."
+    );
   }
 
   const button = document.getElementById(buttonId);
@@ -20,12 +27,11 @@ export function createSpinnerButton(buttonId, formId, onSubmit, onError,
     return;
   }
 
-  const spinner = document.createElement('span');
+  const spinner = document.createElement("span");
   spinner.className = "loader";
-  spinner.style.borderColor =
-      `transparent ${spinnerColor} ${spinnerColor} ${spinnerColor}`;
+  spinner.style.borderColor = `transparent ${spinnerColor} ${spinnerColor} ${spinnerColor}`;
 
-  const buttonLabel = document.createElement('span');
+  const buttonLabel = document.createElement("span");
   buttonLabel.textContent = button.textContent;
   buttonLabel.style.display = "inline";
   buttonLabel.className = "button-label";
@@ -39,11 +45,11 @@ export function createSpinnerButton(buttonId, formId, onSubmit, onError,
 
   button.textContent = "";
 
-  const elements = position === 'right' ? [ buttonLabel, spinner ]
-                                        : [ spinner, buttonLabel ];
-  elements.forEach(element => button.appendChild(element));
+  const elements =
+    position === "right" ? [buttonLabel, spinner] : [spinner, buttonLabel];
+  elements.forEach((element) => button.appendChild(element));
 
-  button.addEventListener('click', (e) => {
+  button.addEventListener("click", (e) => {
     e.preventDefault();
 
     const form = document.getElementById(formId);
@@ -56,11 +62,12 @@ export function createSpinnerButton(buttonId, formId, onSubmit, onError,
     }
 
     onSubmit(data)
-        .then(() => resetButton(button, spinner, buttonLabel,
-                                hideLabelWhileLoading))
-        .catch((error) => {
-          resetButton(button, spinner, buttonLabel, hideLabelWhileLoading);
-          onError?.(error);
-        });
+      .then(() =>
+        resetButton(button, spinner, buttonLabel, hideLabelWhileLoading)
+      )
+      .catch((error) => {
+        resetButton(button, spinner, buttonLabel, hideLabelWhileLoading);
+        onError?.(error);
+      });
   });
 }
