7bdc86f5d546ba4040929c9e8293e0ddc3a56be7
Restyled.io <commits@restyled.io>
2023-05-27 11:34:02 +0000

    Restyled by clang-format

--- spinonsubmit.js
+++ spinonsubmit.js
@@ -4,7 +4,8 @@
  * @param {HTMLElement} button - The button that was clicked to submit the form.
  * @param {HTMLElement} spinner - The spinner element.
  * @param {HTMLElement} buttonLabel - The label of the button.
- * @param {boolean} hideLabelWhileLoading - Whether to hide the label while loading.
+ * @param {boolean} hideLabelWhileLoading - Whether to hide the label while
+ *     loading.
  */
 function resetButton(button, spinner, buttonLabel, hideLabelWhileLoading) {
   spinner.style.display = "none";
@@ -24,14 +25,20 @@ function resetButton(button, spinner, buttonLabel, hideLabelWhileLoading) {
  * @param {string} buttonId - The ID of the button element.
  * @param {string} formId - The ID of the form element.
  * @param {function} onSubmit - The function to run when the form is submitted.
- * @param {function} [onError] - The function to run if an error occurs while submitting the form.
+ * @param {function} [onError] - The function to run if an error occurs while
+ *     submitting the form.
  * @param {string} [spinnerColor='black'] - The color of the spinner.
- * @param {string} [position='left'] - The position of the spinner relative to the label.
- * @param {boolean} [hideLabelWhileLoading=true] - Whether to hide the label while loading.
+ * @param {string} [position='left'] - The position of the spinner relative to
+ *     the label.
+ * @param {boolean} [hideLabelWhileLoading=true] - Whether to hide the label
+ *     while loading.
  */
-export function createSpinnerButton(buttonId, formId, onSubmit, onError, spinnerColor = 'black', position = 'left', hideLabelWhileLoading = true) {
+export function createSpinnerButton(buttonId, formId, onSubmit, onError,
+                                    spinnerColor = 'black', position = 'left',
+                                    hideLabelWhileLoading = true) {
   if (!buttonId || !formId || typeof onSubmit !== 'function') {
-    throw new Error('Missing or incorrect required parameters: buttonId, formId and onSubmit (function) are required.');
+    throw new Error(
+        'Missing or incorrect required parameters: buttonId, formId and onSubmit (function) are required.');
   }
 
   const button = document.getElementById(buttonId);
@@ -42,7 +49,8 @@ export function createSpinnerButton(buttonId, formId, onSubmit, onError, spinner
 
   const spinner = document.createElement('span');
   spinner.className = "loader";
-  spinner.style.borderColor = `transparent ${spinnerColor} ${spinnerColor} ${spinnerColor}`;
+  spinner.style.borderColor =
+      `transparent ${spinnerColor} ${spinnerColor} ${spinnerColor}`;
 
   const buttonLabel = document.createElement('span');
   buttonLabel.textContent = button.textContent;
@@ -57,8 +65,9 @@ export function createSpinnerButton(buttonId, formId, onSubmit, onError, spinner
   }
 
   button.textContent = "";
-  
-  const elements = position === 'right' ? [buttonLabel, spinner] : [spinner, buttonLabel];
+
+  const elements = position === 'right' ? [ buttonLabel, spinner ]
+                                        : [ spinner, buttonLabel ];
   elements.forEach(element => button.appendChild(element));
 
   button.addEventListener('click', (e) => {
@@ -74,10 +83,11 @@ export function createSpinnerButton(buttonId, formId, onSubmit, onError, spinner
     }
 
     onSubmit(data)
-      .then(() => resetButton(button, spinner, buttonLabel, hideLabelWhileLoading))
-      .catch((error) => {
-        resetButton(button, spinner, buttonLabel, hideLabelWhileLoading);
-        onError?.(error);
-      });
+        .then(() => resetButton(button, spinner, buttonLabel,
+                                hideLabelWhileLoading))
+        .catch((error) => {
+          resetButton(button, spinner, buttonLabel, hideLabelWhileLoading);
+          onError?.(error);
+        });
   });
 }
